3cf8e9413ed60589f125003760f33772b8f4fbbb
unknown <trahy032@gmail.com>
2023-11-01 23:18:05 +1030

    added form to contact page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,18 +6,21 @@ export default function Footer() {
   return (
     <div className="footerDiv">
       <ul className="footer">
-          <li className="footerBtn" href="mailto:trahy032@gmail.com">
+        <li className="footerBtn">
+          <a href="mailto:trahy032@gmail.com">
             <MdEmail size={"1.75em"} />
-          </li>
-          <li
-            className="footerBtn"
-            href="http://linkedin.com/in/huy-tran-37a89a1a8"
-          >
+          </a>
+        </li>
+        <li className="footerBtn">
+          <a href="http://linkedin.com/in/huy-tran-37a89a1a8">
             <FaLinkedin size={"1.75em"} />
-          </li>
-          <li className="footerBtn" href="https://github.com/trahy">
+          </a>
+        </li>
+        <li className="footerBtn">
+          <a href="https://github.com/trahy">
             <FaGithub size={"1.75em"} />
-          </li>
+          </a>
+        </li>
       </ul>
     </div>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,17 +4,38 @@ export default function Contact() {
       <h1>Contact Me</h1>
       <div className="contactDiv">
         <form action="">
-          <div className="contactRow">
-            <label className="leftCol" htmlFor="">Name</label>
-            <input className="rightCol" placeholder="Your Name" type="text" required />
-          </div>
-          <div className="contactRow">
-            <label className="leftCol" htmlFor="">Email</label>
-            <input className="rightCol" placeholder="example@email.com" type="text" required />
-          </div>
-          <div className="contactRow">
-            <label className="leftCol" htmlFor="">Message</label>
-            <textarea className="rightCol" placeholder="Enter a message"></textarea>
+          <div className="formDiv">
+            {/* name */}
+            <div className="contactRow">
+              <div className="colLeft">
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="colRight">
+                <input className="textInput" type="text" required />
+              </div>
+            </div>
+            {/* email */}
+            <div className="contactRow">
+              <div className="colLeft">
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="colRight">
+                <input className="textInput" type="email" required />
+              </div>
+            </div>
+            {/* message */}
+            <div className="contactRow">
+              <div className="colLeft">
+                <label htmlFor="message">Message</label>
+              </div>
+              <div className="colRight">
+                <textarea className="textInput" type="text" required />
+              </div>
+            </div>
+            {/* submit button */}
+            <div class="contactRow">
+              <input className="contactBtn btn" type="submit" value="Submit" />
+            </div>
           </div>
         </form>
       </div>
